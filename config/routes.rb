24ebbd78ec7959c982c67b89225757294db24414
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-11 14:17:36 -0500

    new and create works for review

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get "/restaurants/new", to: "restaurants#new", as: "new_restaurant"
   get "/restaurants/:id", to: "restaurants#show", as: "restaurant"
   post "/restaurants", to: "restaurants#create"
-  get "/restaurants/:restaurant_id/reviews/new", to: "reviews#new"
-  post "/restaurants/:restaurant_id/reviews/", to: "reviews#create"
+  get "/restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: 'new_restaurant_review'
+  post "/restaurants/:restaurant_id/reviews", to: "reviews#create", as: 'restaurant_reviews'
 end
