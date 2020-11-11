2c52ac0a1fe68123b6d9be30fbb36aac1d09a83b
renaud archer fortin <renaud.archerf@gmail.com>
2020-11-11 14:38:19 -0500

    challenge done with style

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, :rating, presence: true
   validates :restaurant, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5],
+  validates :rating, inclusion: { in: 0..5,
     message: "%{value} is not a valid rating" }
+  validates :rating, numericality: true
 end
